320e0aabe5702e914cd00485c31d10727c4a2ed3
Devin Price <devingprice@gmail.com>
2019-04-02 19:45:57 -0400

    Add background and styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -122,12 +122,20 @@ var characterBase = {
     },
     updateInfo: function () {
         $('#' + this.name).find(".character-hitpoints").text(this.hitPoints)
-        $('#' + this.name).find(".character-attack").text(this.attackPower)
+        console.log(this.name, 'is',this.role)
+        if (this.role === 'player') {
+            $('#' + this.name).find(".character-attack").eq(1).remove();
+            $('#' + this.name).find(".character-attack").eq(0).html("Attk: <div class='changeText'>" + this.attackPower + '</div')
+        } else if (this.role === 'enemy') {
+            $('#' + this.name).find(".character-attack").eq(1).remove();
+            $('#' + this.name).find(".character-attack").eq(0).text("Counter: " + this.counterAttackPower )
+        } 
+        
         this.removeHealth();
     },
     removeHealth: function () {
         $('#' + this.name).find(".healthbar-value").css('width', (this.hitPoints / this.defaultHitPoints) * 100 + '%')
-        $('#' + this.name).find(".healthbar-value").text(this.hitPoints)
+        $('#' + this.name).find(".healthbar-value").text( this.hitPoints )
         console.log('lost hp', (this.hitPoints / this.defaultHitPoints) * 100)
     }
 }
@@ -270,18 +278,19 @@ function createCharacterCard(character) {
     $('<span>', { class: "character-name" }).text(character.name).appendTo(info);
     //$('<div>', { class: "character-hitpoints" }).text("HP:" + character.hitPoints).appendTo(info);
 
-
-    if (character.role === 'player') {
-        $('<span>', { class: "character-attack" }).text("Attack: " + character.attackPower).appendTo(info)
-    } else if (character.role === 'enemy') {
-        $('<span>', { class: "character-attack" }).text("Counter Attack: " + character.counterAttackPower).appendTo(info)
-    } else {
-        $('<span>', { class: "character-attack" }).text("Attack: " + character.attackPower).appendTo(info)
-        $('<span>', { class: "character-attack" }).text("Counter Attack: " + character.counterAttackPower).appendTo(info)
-    }
-
     var hpShell = $('<div>', { class: "healthbar-shell" }).appendTo(info);
     $('<div>', { class: "healthbar-value" }).text("HP:" + character.hitPoints).appendTo(hpShell);
+
+    if (character.role === 'player') {
+        $('<div>', { class: "character-attack", style: 'display:inline-block'  }).text("Attk:" + character.attackPower ).appendTo(info)
+    } else if (character.role === 'enemy') {
+        $('<div>', { class: "character-attack", style: 'display:inline-block'  }).text("Counter: " + character.counterAttackPower ).appendTo(info)
+    } else {
+        $('<div>', { class: "character-attack", style: 'display:inline-block' }).text("Attk: " + character.attackPower ).appendTo(info)
+        $('<div>', { class: "character-attack", style: 'display:inline-block'  }).text("Counter: " + character.counterAttackPower ).appendTo(info)
+    }
+
+    
     return container;
 }
 function moveAnimate(element, newParent) {
@@ -302,7 +311,7 @@ function moveAnimate(element, newParent) {
         'z-index': 1000
     });
     element.hide();
-    temp.animate({ 'top': newOffset.top, 'left': newOffset.left }, 'slow', function () {
+    temp.animate({ 'top': newOffset.top-80, 'left': newOffset.left }, 'slow', function () {
         element.show();
         temp.remove();
     });
@@ -326,7 +335,7 @@ function selectPlayer(playerName) {
 function selectEnemy(enemyName) {
     console.log('select enemy ' + enemyName)
     enemy = window[enemyName.toLowerCase()]
-    player.role = 'enemy';
+    enemy.role = 'enemy';
     $('#' + enemyName).addClass('enemy');
     moveEnemyToDefenderArea();
 };
@@ -441,39 +450,11 @@ function timedAttack() {
 
 function changePhase(newPhase) {
     phase = newPhase;
-    console.log( phases[newPhase] )
+    console.log( 'new phase:',phases[newPhase] )
     $('#header').html( $('<h1>').text(phases[newPhase]) )
 }
 
 changePhase(0);
-
-/*
-function attack() {
-    //if counter, player.countered()
-
-    enemy.hitPoints -= player.attackPower;
-    player.attackPower += player.defaultAttackPower;
-
-    if (enemy.hitPoints > 0) {
-        player.hitPoints -= enemy.counterAttackPower;
-
-        //anim
-        player.attackAnim();
-        enemy.defAnim();
-    } else {
-        player.attackAnim()
-        enemy.dieAnim();
-    }
-
-
-
-    enemy.updateInfo();
-    player.updateInfo()
-};
-
-*/
-
-
 
 
 //function moveCharactersToEnemySelect() { }
@@ -487,209 +468,3 @@ function moveEnemyToDefenderArea() {
         moveAnimate("#" + enemy.name + '-cont', "#defenderArea");
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-var knight = {
-    role: "player",
-    name: "Knight",
-    images: {
-        idle: 'assets/images/knight/_IDLE_000.png',
-        attack: 'assets/images/knight/ATTACK_007.png',
-        hurt: 'assets/images/knight/_HURT_004.png',
-        die: 'assets/images/knight/_DIE_006.png'
-    },
-    hitPoints: 120,
-    defaultHitPoints: 120,
-    attackPower: 8,
-    defaultAttackPower: 8,
-    counterAttackPower: 35,
-    attackAnim: function (directionMove) {
-        var refer = this;
-        var first = "+=200";
-        var second = "-=200";
-        if (directionMove === "left") {
-            first = "-=200";
-            second = "+=200";
-        }
-        $("#" + this.name).find("img").eq(0)//.attr('src', refer.images.attack)
-            .queue(function (next) { $(this).attr('src', refer.images.attack); next() })
-            .animate({ left: first }, 200)
-            .animate({ left: second }, 300)
-            .queue(function (next) { $(this).attr('src', refer.images.idle); next() })
-    },
-    defAnim: function (directionHit) {
-        var refer = this;
-        var first = "+=100";
-        var second = "-=100";
-        if (directionHit === "left") {
-            first = "-=100";
-            second = "+=100";
-        }
-        setTimeout(function () {
-            $('#' + refer.name)//.delay(500)
-                .find("img").eq(0).attr('src', refer.images.hurt)
-                .animate({ left: first }, 200)
-                .animate({ left: second }, 100)
-                .queue(function (next) { $(this).attr('src', refer.images.idle); next() })
-        }, 200)
-
-    },
-    dieAnim: function (directionHit) {
-        var refer = this;
-        var first = "+=100";
-        if (directionHit === "left") {
-            first = "-=100";
-        }
-        setTimeout(function () {
-            $('#' + refer.name)//.delay(500)
-                .find("img").eq(0).attr('src', refer.images.hurt)
-                .animate({ left: first }, 200)
-                .queue(function (next) {
-                    $(this).removeClass('enemy');
-                    $(this).attr({
-                        'src': refer.images.die,
-                        "class": 'character-img dead'
-                    });
-                    //$(this).attr('src', refer.images.die);
-                    next()
-                })
-        }, 200)
-    },
-    counterAnim: function(){
-        setTimeout(function () {
-            $('#' + refer.name)//.delay(500)
-                .find("img").eq(0).attr('src', refer.images.hurt)
-                .animate({ left: first }, 200)
-                .animate({ left: second }, 150)
-                .queue(function (next) { $(this).attr('src', refer.images.idle); next() })
-        }, 500)
-    },
-    counteredAnim: function(){
-        setTimeout(function () {
-            $('#' + refer.name)//.delay(500)
-                .find("img").eq(0).attr('src', refer.images.hurt)
-                .animate({ left: first }, 200)
-                .animate({ left: second }, 150)
-                .queue(function (next) { $(this).attr('src', refer.images.idle); next() })
-        }, 500)
-    },
-    updateInfo: function () {
-        $('#' + this.name).find(".character-hitpoints").text(this.hitPoints)
-        $('#' + this.name).find(".character-attack").text(this.attackPower)
-        this.removeHealth();
-    },
-    removeHealth: function () {
-        $('#' + this.name).find(".healthbar-value").css('width', (this.hitPoints / this.defaultHitPoints) * 100 + '%')
-        $('#' + this.name).find(".healthbar-value").text(this.hitPoints)
-        console.log('lost hp', (this.hitPoints / this.defaultHitPoints) * 100)
-    }
-}
-
-var orc = {
-    role: "enemy",
-    name: "orc",
-    images: {
-        idle: 'assets/images/orc/IDLE_000.png',
-        attack: 'assets/images/orc/ATTAK_005.png',
-        hurt: 'assets/images/orc/HURT_004.png',
-        die: 'assets/images/orc/DIE_006.png'
-    },
-    hitPoints: 100,
-    defaultHitPoints: 100,
-    attackPower: 8,
-    defaultAttackPower: 8,
-    counterAttackPower: 35,
-    attackAnim: function (directionMove) {
-        var refer = this;
-        var first = "+=200";
-        var second = "-=200";
-        if (directionMove === "left") {
-            first = "-=200";
-            second = "+=200";
-        }
-        $("#" + this.name).find("img").eq(0)//.attr('src', refer.images.attack)
-            .queue(function (next) { $(this).attr('src', refer.images.attack); next() })
-            .animate({ left: first }, 200)
-            .animate({ left: second }, 300)
-            .queue(function (next) { $(this).attr('src', refer.images.idle); next() })
-    },
-    defAnim: function (directionHit) {
-        var refer = this;
-        var first = "+=100";
-        var second = "-=100";
-        if (directionHit === "left") {
-            first = "-=100";
-            second = "+=100";
-        }
-        setTimeout(function () {
-            $('#' + refer.name)//.delay(500)
-                .find("img").eq(0).attr('src', refer.images.hurt)
-                .animate({ left: first }, 200)
-                .animate({ left: second }, 100)
-                .queue(function (next) { $(this).attr('src', refer.images.idle); next() })
-        }, 200)
-
-    },
-    dieAnim: function (directionHit) {
-        var refer = this;
-        var first = "+=100";
-        if (directionHit === "left") {
-            first = "-=100";
-        }
-        setTimeout(function () {
-            $('#' + refer.name)//.delay(500)
-                .find("img").eq(0).attr('src', refer.images.hurt)
-                .animate({ left: first }, 200)
-                .queue(function (next) {
-                    $(this).removeClass('enemy');
-                    $(this).attr({
-                        'src': refer.images.die,
-                        "class": 'character-img dead'
-                    });
-                    //$(this).attr('src', refer.images.die);
-                    next()
-                })
-        }, 200)
-    },
-    counterAnim: function(){
-        setTimeout(function () {
-            $('#' + refer.name)//.delay(500)
-                .find("img").eq(0).attr('src', refer.images.hurt)
-                .animate({ left: first }, 200)
-                .animate({ left: second }, 150)
-                .queue(function (next) { $(this).attr('src', refer.images.idle); next() })
-        }, 500)
-    },
-    counteredAnim: function(){
-        setTimeout(function () {
-            $('#' + refer.name)//.delay(500)
-                .find("img").eq(0).attr('src', refer.images.hurt)
-                .animate({ left: first }, 200)
-                .animate({ left: second }, 150)
-                .queue(function (next) { $(this).attr('src', refer.images.idle); next() })
-        }, 500)
-    },
-    updateInfo: function () {
-        $('#' + this.name).find(".character-hitpoints").text(this.hitPoints)
-        $('#' + this.name).find(".character-attack").text(this.attackPower)
-        this.removeHealth();
-    },
-    removeHealth: function () {
-        $('#' + this.name).find(".healthbar-value").css('width', (this.hitPoints / this.defaultHitPoints) * 100 + '%')
-        $('#' + this.name).find(".healthbar-value").text(this.hitPoints)
-        console.log('lost hp', (this.hitPoints / this.defaultHitPoints) * 100)
-    }
-}
-*/
